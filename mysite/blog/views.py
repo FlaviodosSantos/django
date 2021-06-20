aae80d6993f0b3751c7fe412282e16bd0a9e62da
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-06-20 09:28:12 -0300

    mysite/blog: finalizando tutorial e adionando paginação

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 2 #paginação
 
 class PostDetail(generic.DeleteView):
     model = Post
