02013bb0e539b2aa0bd2d93aca948ad25f768a1d
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-06 21:57:53 -0300

    add pastas media, template, static #gestao_cliente

--- gestao_clientes/clientes/models.py
+++ gestao_clientes/clientes/models.py
@@ -1,3 +1,14 @@
+from typing import no_type_check_decorator
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    salary = models.DecimalField(max_digits=5, decimal_places=2)
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
+    
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
