718dea830cd80bc78f26bd4629203001a0e3969e
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-11 23:51:07 -0300

    setings e urls cadastro_login

--- cadastro_login/cadastro_login/settings.py
+++ cadastro_login/cadastro_login/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    # 3rd party
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -125,4 +131,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # django-allauth
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
 
+SITE_ID = 1
--- cadastro_login/cadastro_login/urls.py
+++ cadastro_login/cadastro_login/urls.py
@@ -17,5 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Django Admin
+    path("admin/", admin.site.urls),
+    # User Management
+    path("accounts/", include("allauth.urls")),
+    # Local
+    path("", include("pages.urls", namespace="pages")),
 ]
