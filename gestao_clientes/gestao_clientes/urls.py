4f2698e23c59aee6bed7a0161ebe5bb7f479d0b0
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-14 19:26:39 -0300

    mod urls para exibir medias #gestao_clientes

--- gestao_clientes/gestao_clientes/urls.py
+++ gestao_clientes/gestao_clientes/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# pro django poder exibir arquivos de media
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
